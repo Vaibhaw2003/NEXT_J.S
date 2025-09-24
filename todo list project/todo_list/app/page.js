2aaf6571a37dd140618a545ecc0d48efe5d47643
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-25 01:06:28 +0530

    done

--- todo list project/todo_list/app/page.js
+++ todo list project/todo_list/app/page.js
@@ -1,103 +1,87 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const page = () => {
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [text, setText] = useState("")
+  const [dec, setDec] = useState("")
+  const [time, setTime] = useState("")
+
+  const [main, setMain] = useState([])
+
+  const submithandl =(e)=>{
+    e.preventDefault()
+    setMain([...mainTask, {text,dec,time}]);
+    setText(" ")
+    setDec(" ")
+    setTime(" ")
+    console.log(mainTask)
+    
+  };
+  let renderTask =<h3>no data available</h3>
+
+  renderTask = mainTask.map((t,i )=>{
+    return (  
+    <div className='flex justify-between '>
+      <h4>{t.text}</h4>
+      <h4>{t.dec}</h4>
+      <h4>{t.time}</h4>
     </div>
-  );
+    );
+  });
+
+
+  return (
+    <div>
+      <>
+      <h1 className='text-white bg-black text-5xl text-center p-5 font-bold  '>MY TODO LIST</h1>
+
+      <form onSubmit={submithandl}>
+        <input type='text'  className='text-2xl border-b-blue-800 text-black bg-amber-200 border-2 m-5 px-4 py-2 '
+        placeholder = "enter text"
+
+        value={text}
+        onChange={(e)=>{
+          setText(e.target.value)
+        }}
+
+        />
+
+         <input type='text'  className='text-2xl border-b-blue-800 text-black bg-amber-200 border-2 m-5 px-4 py-2 '
+        placeholder = "enter descripton"
+
+        value={dec}
+        onChange={(e)=>{
+          setDec(e.target.value)
+        }}
+
+        />
+
+         <input type='text'  className='text-2xl border-b-blue-800 text-black bg-amber-200 border-2 m-5 px-4 py-2 '
+        placeholder = "enter time"
+
+        value={time}
+        onChange={(e)=>{
+          setTime(e.target.value)
+        }}
+
+        />
+
+        <button className = 'bg-emerald-400 rounded-1xl text-3xl px-2 py-3 m-5 font-bold '>ADD TEXT</button>
+        
+      </form>
+      <hr />
+      <div className='p-4 bg-black text-white items-center'>
+        <ul>
+          {renderTask}
+        </ul>
+
+
+      </div>
+      
+      </>
+    </div>
+  )
 }
+
+export default page
